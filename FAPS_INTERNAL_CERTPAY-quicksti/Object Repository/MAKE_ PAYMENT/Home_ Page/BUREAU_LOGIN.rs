45c0c01b63244432c205115213a2b17b749b57bb
PratyushaNulu <pratyusha.nulu@zenq.com>
2019-09-25 15:52:38 +0530

    Added data file

---
+++ FAPS_INTERNAL_CERTPAY-quicksti/Object Repository/MAKE_ PAYMENT/Home_ Page/BUREAU_LOGIN.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>BUREAU_LOGIN</name>
+   <tag></tag>
+   <elementGuidId>5ac10f02-d77e-42c9-ac2f-68ea3c714a67</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@class='navlinks navlinks-button']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
